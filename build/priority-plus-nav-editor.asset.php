0eea78f4b768a29cbe187502b37dec628f2a043a
Troy Chaplin <troy.chaplin@gmail.com>
2026-01-02 18:35:47 -0500

    change: depth indentation styles

--- build/priority-plus-nav-editor.asset.php
+++ build/priority-plus-nav-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '040298b306aa1a58ba3b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '51a330a400fb321176cf');
